a2b9bf49796cc4f0c58ebf7d97ea68f9fee9d45c
Manuel Garcia <alexgarcia2k@gmail.com>
2020-06-18 19:02:01 -0700

    implementation of delete.

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,56 @@
 import 'source-map-support/register'
-
+import * as AWS  from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+
+const docClient = createDynamoDbClient();
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  console.log("Deleting id  : " + todoId)
+  var params = {
+  TableName:todosTable,
+    Key : {
+        "userId": "mgarcia",
+        "todoId": todoId
+    }
+  };
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  console.log("Attempting a conditional delete...");
+  const result = await docClient.delete(params).promise()
+  console.log(JSON.stringify(result))
+  if (result.ConsumedCapacity == undefined) {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: ""
+    }
+  }
+  // Item doesn't exist
+  return {
+    statusCode: 404,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      error: 'Todo does not exist '
+    })
+  }
+
+}
+
+
+
+function createDynamoDbClient(){
+  if(process.env.IS_OFFLINE) {
+    console.log("Creating a local dynamo db instance")
+    return new AWS.DynamoDB.DocumentClient({
+      region : 'localhost',
+      endpoint : 'http://localhost:8000'
+    })
+  }
+
+  return new AWS.DynamoDB.DocumentClient();
 }
