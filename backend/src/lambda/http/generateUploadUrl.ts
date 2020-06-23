b2f03cb95d00d52f7f53ae20a14724250cb07ddb
Manuel Garcia <alexgarcia2k@gmail.com>
2020-06-23 15:01:18 -0700

    Add generate upload url.

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,11 +1,25 @@
 import 'source-map-support/register'
-
+import * as AWS from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  //const todoId = event.pathParameters.todoId
-  event.pathParameters.todoId
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+export const s3 = new AWS.S3({signatureVersion: 'v4'})
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId;
+  const signedUrlExpireSeconds = 60 * 5;
+
+  const s3Url = s3.getSignedUrl('putObject', {
+    Bucket: process.env.IMAGES_S3_BUCKET,
+    Key: todoId,
+    Expires: signedUrlExpireSeconds,
+  });
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({uploadUrl : s3Url})
+  }
 }
